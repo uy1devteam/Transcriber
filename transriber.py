512d2414bf8dc653a966e9315f1dbedc50c0cb3a
ivanange <ivanangethe1@gmail.com>
2023-01-09 15:13:57 +0100

    optimized transcribe method

--- transriber.py
+++ transriber.py
@@ -3,11 +3,6 @@ import streamlit as st
 import whisper
 
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-
 @st.cache
 def transcribe(file: BytesIO):
     # save to audios dir with unique name
@@ -16,24 +11,26 @@ def transcribe(file: BytesIO):
         f.write(file.getbuffer())
 
     model = whisper.load_model("small")
+    result = model.transcribe(filename)
+	
+    # # load audio and pad/trim it to fit 30 seconds
+    # audio = whisper.load_audio(filename)
+    # audio = whisper.pad_or_trim(audio)
 
-    # load audio and pad/trim it to fit 30 seconds
-    audio = whisper.load_audio(filename)
-    audio = whisper.pad_or_trim(audio)
+    # # make log-Mel spectrogram and move to the same device as the model
+    # mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    # make log-Mel spectrogram and move to the same device as the model
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    # # detect the spoken language
+    # # _, probs = model.detect_language(mel)
+    # # print(f"Detected language: {max(probs, key=probs.get)}")
 
-    # detect the spoken language
-    # _, probs = model.detect_language(mel)
-    # print(f"Detected language: {max(probs, key=probs.get)}")
+    # # decode the audio
+    # options = whisper.DecodingOptions(without_timestamps=True, fp16=False)
+    # result = model.decode(mel, options)
 
-    # decode the audio
-    options = whisper.DecodingOptions(without_timestamps=True, fp16=False)
-    result = model.decode(mel, options)
-
-    # print the recognized text
-    return result.text
+    # # print the recognized text
+    # return result.text
+    return result["text"]
 
 
 st.title('Transcriber')
